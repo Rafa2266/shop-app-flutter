4f90f3a5d847384f14b9aba3baeb868e222142eb
Rafael Franca Scofield Lauar <rafavasco12@hotmail.com>
2025-09-21 11:51:08 -0300

    adiocionar os pedidos no banco de dados

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,7 @@
 class Constants {
   static const productBaseUrl =
       'https://shop-coder2-default-rtdb.firebaseio.com/products';
+
+  static const orderBaseUrl =
+      'https://shop-coder2-default-rtdb.firebaseio.com/orders';
 }
