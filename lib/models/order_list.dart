aa3283f35403701265ac5b40944e04ad773b9b3c
Rafael Franca Scofield Lauar <rafavasco12@hotmail.com>
2025-09-21 13:04:53 -0300

    carrregar pedidos feitos do banco de dados

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shop/models/cart.dart';
+import 'package:shop/models/cart_item.dart';
 import 'package:shop/models/order.dart';
 import 'package:shop/utils/constants.dart';
 
@@ -17,10 +18,35 @@ class OrderList with ChangeNotifier {
     return _items.length;
   }
 
+  Future<void> loadOrders() async {
+    _items.clear();
+    final response = await http.get(
+      Uri.parse('${Constants.orderBaseUrl}.json'),
+    );
+    if (response.body == 'null') return;
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((orderId, orderData) {
+      _items.add(Order(
+        id: orderId,
+        total: orderData['total'],
+        date: DateTime.parse(orderData['date']),
+        products: (orderData['products'] as List<dynamic>)
+            .map((item) => CartItem(
+                id: item['id'],
+                productId: item['productId'],
+                name: item['name'],
+                quantity: item['quantity'],
+                price: item['price']))
+            .toList(),
+      ));
+    });
+    notifyListeners();
+  }
+
   Future<void> addOrder(Cart cart) async {
     final date = DateTime.now();
     final response = await http.post(
-      Uri.parse(Constants.orderBaseUrl + '.json'),
+      Uri.parse('${Constants.orderBaseUrl}.json'),
       body: jsonEncode(
         {
           "total": cart.totalAmount,
