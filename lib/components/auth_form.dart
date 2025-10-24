dadc01cc38c8042b0784b546793a5dadc480dca5
Rafael Franca Scofield Lauar <rafavasco12@hotmail.com>
2025-10-23 23:33:48 -0300

    adicionado exibição de erros no login e cadastro de usuario

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -1,6 +1,9 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/exceptions/auth_exception.dart';
+import 'package:shop/models/auth.dart';
 
 enum AuthMode { signup, login }
 
@@ -30,7 +33,21 @@ class _AuthFormState extends State<AuthForm> {
     });
   }
 
-  void _submit() {
+  _showErrorDialog(String msg) {
+    showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+              title: const Text("Ocorreu um erro!!"),
+              content: Text(msg),
+              actions: [
+                TextButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: const Text('Fechar'))
+              ],
+            ));
+  }
+
+  Future<void> _submit() async {
     final isValid = _formkey.currentState?.validate() ?? false;
 
     if (!isValid) {
@@ -42,10 +59,19 @@ class _AuthFormState extends State<AuthForm> {
 
     _formkey.currentState?.save();
 
-    if (_isLogin()) {
-      //login
-    } else {
-      //registrar
+    Auth auth = Provider.of(context, listen: false);
+    try {
+      if (_isLogin()) {
+        //login
+        await auth.login(_authData['email']!, _authData['password']!);
+      } else {
+        //registrar
+        await auth.signup(_authData['email']!, _authData['password']!);
+      }
+    } on AuthException catch (e) {
+      _showErrorDialog(e.toString());
+    } catch (e) {
+      _showErrorDialog('Ocorreu um erro inédito!');
     }
 
     setState(() {
